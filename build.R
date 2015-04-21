960d83f0b5f6eac8456a58fe8a0dec4c11730d98
Carson Sievert <cpsievert1@gmail.com>
2015-04-21 16:48:17 -0500

    Fix #5

--- build.R
+++ build.R
@@ -1,14 +1,15 @@
 local({
   if (!file.exists('_config.yml')) return()
   x = iconv(readLines('_config.yml', encoding = 'UTF-8'), 'UTF-8')
+  baseurl = grep('^baseurl:', x, value = TRUE)
+  baseurl = sub(".*['\"](.*)['\"].*", "\\1",  baseurl)
   x = grep('^markdown:\\s*[a-z]+\\s*$', x, value = TRUE)
   # see if we need to use the Jekyll render in knitr (i.e. if the markdown
   # engine is kramdown)
   if (length(x) == 0 || (length(x) == 1 && strsplit(x, ':\\s*')[[1]][2] == 'kramdown')) {
     knitr::render_jekyll()
   } else knitr::render_markdown()
-})
-local({
+  
   # input/output filenames are passed as two additional arguments to Rscript
   a = commandArgs(TRUE)
   d = gsub('^_|[.][a-zA-Z]+$', '', a[1])
@@ -27,7 +28,7 @@ local({
       base.url = 'http://db.yihui.name/jekyll/'
     )
   } else {
-    knitr::opts_knit$set(base.url = '/')
+    knitr::opts_knit$set(base.url = baseurl)
   }
   knitr::opts_knit$set(width = 70)
   knitr::knit(a[1], a[2], quiet = TRUE, encoding = 'UTF-8', envir = .GlobalEnv)
